02cc9e8ac3781062d309432aca12a847b454f0f2
jarbus <jarbus@tutanota.com>
2020-05-15 15:00:54 -0400

    ai starts games as players

--- benedict/gameController.py
+++ benedict/gameController.py
@@ -1,20 +1,32 @@
 import socketio
+import time
 
 sio = socketio.Client()
-sio.connect('http://localhost:8080')
-#sio.emit('connect',{'foo':'bar'})
 
-# sio.emit('registerplayer', {
-#     "playerName": "benedict",
-#     "playerId": 2
-# })
-# sio.emit('registerplayer', {
-#     "playerName": "arnold",
-#     "playerId": 3
-# })
+my_state = None
+
+@sio.event
+def state(data):
+    print(data)
+    global my_state
+    my_state = data
+    print(my_state['stateId'])
+
+sio.connect('http://localhost:8080')
+sio2 = socketio.Client()
+sio2.connect('http://localhost:8080')
+
+sio.emit('registerplayer', {
+    "playerName": "benedict",
+    "playerId": 2
+})
+sio2.emit('registerplayer', {
+    "playerName": "arnold",
+    "playerId": 3
+})
 sio.emit('create',{
     'gameName': '1',
-    'playerName': '',
+    'playerName': 'benedict',
     'password': ''
     })
 
@@ -23,3 +35,26 @@ sio.emit('join',{
    'playerName': 'benedict',
    'password': ''
    })
+sio2.emit('join',{
+  'gameName': '1',
+  'playerName': 'arnold',
+  'password': ''
+  })
+
+#time.sleep(1)
+#sio2.emit('command',{
+#    'command':'ready',
+#    'stateId':str(my_state['stateId'])
+#    })
+
+time.sleep(1)
+sio.emit('command',{
+    'command': 'start',
+    #'action':'',
+    #'target':'',
+    #'blockingRole': '',
+    #'role':'',
+    #'roles':[''],
+    'gameType':'original',
+    'stateId':str(my_state['stateId'])
+    })
