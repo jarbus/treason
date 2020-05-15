4ada084017cb469bc2a107d4376a94bfe644e40c
jarbus <jarbus@tutanota.com>
2020-05-15 13:24:05 -0400

    socket progress, creating and joining games

--- benedict/gameController.py
+++ benedict/gameController.py
@@ -1,18 +1,25 @@
 import socketio
-# data = {
-#     'command': "challenge",        # The name of the command: play-action, block, challenge, allow, reveal, exchange
-#     'action': "",         # For the play-action command, the action to play
-#     'target': 0,          # When playing an action which targets another player, the index of the player to target
-#     'blockingRole': "",   # For the block command, the role to block with
-#     'role': "",           # For the reveal command, the role to reveal
-#     'roles': [""],        # For the exchange command, the role(s) you wish to keep
-#     'stateId': 1          # Must match the stateId from the latest game state
-# }
 
-target_host = "localhost"
+sio = socketio.Client()
+sio.connect('http://localhost:8080')
+#sio.emit('connect',{'foo':'bar'})
 
-target_port = 8080  # create a socket object
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# sio.emit('registerplayer', {
+#     "playerName": "benedict",
+#     "playerId": 2
+# })
+# sio.emit('registerplayer', {
+#     "playerName": "arnold",
+#     "playerId": 3
+# })
+sio.emit('create',{
+    'gameName': '1',
+    'playerName': '',
+    'password': ''
+    })
 
-# connect the client
-client.connect((target_host,target_port))
+sio.emit('join',{
+   'gameName': '1',
+   'playerName': 'benedict',
+   'password': ''
+   })
